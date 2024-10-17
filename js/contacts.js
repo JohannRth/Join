392a3c5453ee40832db05674e99487b486282469
JohannRth <johann.roth.e46@googlemail.com>
2024-10-17 11:43:31 +0200

    add contacts JSON

---
+++ js/contacts.js
@@ -0,0 +1,34 @@
+// Array mit den Kontakten
+const contacts = [
+    {
+        'name': 'Anton Mayer',
+        'email': 'antom@gmail.com',
+        'phone': '+49 176 12345678'
+    },
+    {
+        'name': 'Anja Schulz',
+        'email': 'schulz@hotmail.com',
+        'phone': '+49 176 23456789'
+    },
+    {
+        'name': 'Benedikt Ziegler',
+        'email': 'benedikt@gmail.com',
+        'phone': '+49 176 34567890'
+    },
+    {
+        'name': 'David Eisenberg',
+        'email': 'davidberg@gmail.com',
+        'phone': '+49 176 45678901'
+    },
+    {
+        'name': 'Eva Fischer',
+        'email': 'eva@gmail.com',
+        'phone': '+49 176 56789012'
+    },
+    {
+        'name': 'Emmanuel Mauer',
+        'email': 'emmanuelma@gmail.com',
+        'phone': '+49 176 67890123'
+    }
+];
+
